eceed7d890f8d82d77804a98641780fa3ce4574f
paulapivat <paul.apivat@gmail.com>
2019-03-01 21:33:15 +0700

    add basic layout for InvoiceList

--- client/src/views/InvoiceList/index.js
+++ client/src/views/InvoiceList/index.js
@@ -1,7 +1,19 @@
+// import packages
 import React, { Component } from "react";
+
+//import styles
+import "./InvoiceList.css";
 
 export default class index extends Component {
   render() {
-    return <div> Invoice List </div>;
+    return (
+      <div className="invoice-list-container">
+        {" "}
+        Invoice List Tada
+        <div className="top-nav">Top Navigation Placeholder</div>
+        <div className="sidebar-nav">Sidebar Navigation placeholder</div>
+        <div className="new-invoice-button">Add a new Invoice Button</div>
+      </div>
+    );
   }
 }
