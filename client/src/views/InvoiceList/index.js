7a570e0bc1f9cbf7ef35b5990748f323a04e7896
paulapivat <paul.apivat@gmail.com>
2019-03-02 14:25:35 +0700

    add top, mid, footer section invoice-list-page layout

--- client/src/views/InvoiceList/index.js
+++ client/src/views/InvoiceList/index.js
@@ -7,12 +7,14 @@ import "./InvoiceList.css";
 export default class index extends Component {
   render() {
     return (
-      <div className="invoice-list-container">
+      <div className="main-container">
         {" "}
-        Invoice List Tada
         <div className="top-nav">Top Navigation Placeholder</div>
-        <div className="sidebar-nav">Sidebar Navigation placeholder</div>
-        <div className="new-invoice-button">Add a new Invoice Button</div>
+        <div className="mid-container">
+          <div className="sidebar-nav">Sidebar Nav</div>
+          <div className="invoice-list-container">Invoice List Container</div>
+        </div>
+        <div className="footer">Footer</div>
       </div>
     );
   }
