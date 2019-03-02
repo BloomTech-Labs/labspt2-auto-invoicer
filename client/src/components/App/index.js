23eec40574c978b0112b8024ba5ccd32b749cc16
paulapivat <paul.apivat@gmail.com>
2019-03-03 01:17:25 +0700

    add structure for CreateInvoice component without styling

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -5,10 +5,16 @@ import React, { Component } from "react";
 import "./App.css";
 
 //imported components
+import CreateInvoice from "../views/CreateInvoice";
 
 class App extends Component {
   render() {
-    return <div className="App">App Content</div>;
+    return (
+      <div className="App">
+        App Content
+        <CreateInvoice />
+      </div>
+    );
   }
 }
 
---
+++ client/src/components/views/CreateInvoice/index.js
@@ -0,0 +1,55 @@
+// import packages
+import React, { Component } from "react";
+
+//import styles
+import "./CreateInvoice.css";
+
+//import components
+//import CreateInvoiceButton from "../../components/reusableComponents/CreateInvoiceButton";
+
+export default class index extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        {" "}
+        <div className="top-nav">
+          <div className="top-nav-left">
+            <ul className="breadcrumb">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Invoices</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="top-nav-right">
+            <div className="credits">Credits: 3</div>
+            <div className="signout-button">Sign Out</div>
+          </div>
+        </div>
+        <div className="mid-container">
+          <div className="sidebar-nav">
+            <div className="sidebar-nav-button">Invoice</div>
+            <div className="sidebar-nav-button">Billing</div>
+            <div className="sidebar-nav-button">Settings</div>
+          </div>
+          <div className="invoice-list-container">
+            <div className="invoice-box">
+              <div className="invoice-box-top">Invoice Number</div>
+              <div className="invoice-box-mid">Invoice Content</div>
+              <div className="invoice-box-bottom">Created: 3-2-2019</div>
+            </div>
+            <div className="invoice-box">Sample Invoice 2</div>
+            <div className="invoice-box">Sample Invoice 3</div>
+            <div className="invoice-box" />
+            <div className="invoice-box">Sample Invoice 5</div>
+            <div className="invoice-box">Sample Invoice 6</div>
+          </div>
+        </div>
+        <div className="footer">Footer</div>
+      </div>
+    );
+  }
+}
