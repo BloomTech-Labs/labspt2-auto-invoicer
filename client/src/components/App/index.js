da12b979c603a51d0a8da99eccd0d7df2141bf7d
Joseph Thompson <joe.thompson838@gmail.com>
2019-03-03 23:22:58 -0500

    added comments to App component for sign in modal

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -7,17 +7,20 @@ import "./App.css";
 //imported components
 import SideNavigation from "../SideNavigation";
 import SignInModal from "../SignInModal";
+
 class App extends Component {
   state = {
     toggleSignIn: false
   };
   signInModal = () => {
+    // return the opposite of the current state of toggleSignIn
     return this.setState({ toggleSignIn: !this.state.toggleSignIn });
   };
   render() {
     return (
       <div className="App">
         <SideNavigation signInModal={this.signInModal} />
+        {/* check if sigin clicked and open up signin modal or visa-versa */}
         {this.state.toggleSignIn ? (
           <SignInModal click={this.signInModal} />
         ) : null}
