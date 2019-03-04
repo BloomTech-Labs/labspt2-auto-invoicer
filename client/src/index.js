b2d2247b058b027d9d2289b6af36449754167c2f
Dewayne87 <dewaynelindsay87@gmail.com>
2019-03-04 03:32:29 -0800

    changed render back to app

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-import SettingsPage from "./views/SettingsPage"
+
 
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<SettingsPage />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
