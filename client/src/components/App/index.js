39df84001b9d23df753bfc55b722a1dabccd6791
Joseph Thompson <joe.thompson838@gmail.com>
2019-03-03 21:33:44 -0500

    App Component settings to have side nav and login button working

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -5,10 +5,24 @@ import React, { Component } from "react";
 import "./App.css";
 
 //imported components
-
+import SideNavigation from "../SideNavigation";
+import SignInModal from "../SignInModal";
 class App extends Component {
+  state = {
+    toggleSignIn: false
+  };
+  signInModal = () => {
+    return this.setState({ toggleSignIn: !this.state.toggleSignIn });
+  };
   render() {
-    return <div className="App">App Content</div>;
+    return (
+      <div className="App">
+        <SideNavigation signInModal={this.signInModal} />
+        {this.state.toggleSignIn ? (
+          <SignInModal click={this.signInModal} />
+        ) : null}
+      </div>
+    );
   }
 }
 
