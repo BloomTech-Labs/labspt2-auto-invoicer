d8a37e4340d58e7850f345ea7531f528437e3aee
Joseph Thompson <joe.thompson838@gmail.com>
2019-03-04 00:53:02 -0500

    added basics of signup form and icons to match

--- client/src/components/SignUpForm/index.js
+++ client/src/components/SignUpForm/index.js
@@ -30,7 +30,11 @@ class SignUpForm extends React.Component {
   comparePasswords = (pass1, pass2) => {
     return pass1 === pass2 ? true : false;
   };
+<<<<<<< HEAD
   createRegisterObject = e => {
+=======
+  createLoginObject = e => {
+>>>>>>> added basics of signup form and icons to match
     e.preventDefault();
     const {
       email,
@@ -58,9 +62,15 @@ class SignUpForm extends React.Component {
   };
   render() {
     return (
+<<<<<<< HEAD
       <form className="signup-form" onSubmit={this.createRegisterObject}>
         <label htmlFor="email" className="signup-form-group">
           <EmailIcon className="signup-icon" />
+=======
+      <form className="signin-form" onSubmit={this.createLoginObject}>
+        <label htmlFor="email" className="signin-form-group">
+          <EmailIcon />
+>>>>>>> added basics of signup form and icons to match
           <input
             className="signin-input"
             type="text"
@@ -71,8 +81,13 @@ class SignUpForm extends React.Component {
             value={this.state.email}
           />
         </label>
+<<<<<<< HEAD
         <label htmlFor="firstName" className="signup-form-group">
           <PersonIcon className="signup-icon" />
+=======
+        <label htmlFor="firstName" className="signin-form-group">
+          <PersonIcon />
+>>>>>>> added basics of signup form and icons to match
           <input
             className="signin-input"
             type="text"
@@ -83,8 +98,13 @@ class SignUpForm extends React.Component {
             value={this.state.firstName}
           />
         </label>
+<<<<<<< HEAD
         <label htmlFor="lastName" className="signup-form-group">
           <Person2Icon className="signup-icon" />
+=======
+        <label htmlFor="lastName" className="signin-form-group">
+          <Person2Icon />
+>>>>>>> added basics of signup form and icons to match
           <input
             className="signin-input"
             type="text"
@@ -95,8 +115,13 @@ class SignUpForm extends React.Component {
             value={this.state.lastName}
           />
         </label>
+<<<<<<< HEAD
         <label htmlFor="phone" className="signup-form-group">
           <PhoneIcon className="signup-icon" />
+=======
+        <label htmlFor="phone" className="signin-form-group">
+          <PhoneIcon />
+>>>>>>> added basics of signup form and icons to match
           <input
             className="signin-input"
             type="text"
@@ -107,8 +132,13 @@ class SignUpForm extends React.Component {
             value={this.state.phone}
           />
         </label>
+<<<<<<< HEAD
         <label htmlFor="password" className="signup-form-group">
           <PasswordIcon className="signup-icon" />
+=======
+        <label htmlFor="password" className="signin-form-group">
+          <PasswordIcon />
+>>>>>>> added basics of signup form and icons to match
           <input
             className="signin-input"
             type="password"
@@ -119,8 +149,13 @@ class SignUpForm extends React.Component {
             value={this.state.password}
           />
         </label>
+<<<<<<< HEAD
         <label htmlFor="password2" className="signup-form-group">
           <Password2Icon className="signup-icon" />
+=======
+        <label htmlFor="password2" className="signin-form-group">
+          <Password2Icon />
+>>>>>>> added basics of signup form and icons to match
           <input
             className="signin-input"
             type="password"
@@ -131,7 +166,11 @@ class SignUpForm extends React.Component {
             value={this.state.password2}
           />
         </label>
+<<<<<<< HEAD
         <button className="authentication-btns register-btn" type="submit">
+=======
+        <button className="authentication-btns" type="submit">
+>>>>>>> added basics of signup form and icons to match
           Register
         </button>
       </form>
