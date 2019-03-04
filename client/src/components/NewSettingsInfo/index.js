0041180ff35dec568d04fc71d22fd787e5ba194c
Dewayne87 <dewaynelindsay87@gmail.com>
2019-03-04 03:28:36 -0800

    finished settings page layout added confirm password check and min length of 8 charachters

---
+++ client/src/components/NewSettingsInfo/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+// import css here
+import "./NewSettingsInfo.css";
+import Button from "../reusableComponents/Button"
+const NewSettingsInfo = props => {
+  const {
+    email,
+    oldPassword,
+    newPassword,
+    confirmPassword
+  } = props.settingsInfo;
+  const { changeHandler } = props;
+  return (
+    <fieldset className="settings_info">
+      <input
+        type="email"
+        onChange={changeHandler}
+        name="email"
+        value={email}
+        placeholder="Email Address"
+      />
+      <input
+        type="password"
+        onChange={changeHandler}
+        name="oldPassword"
+        value={oldPassword}
+        placeholder="Old Password"
+      />
+      <input
+        type="password"
+        onChange={changeHandler}
+        name="newPassword"
+        value={newPassword}
+        placeholder="New Password"
+      />
+      <input
+        type="password"
+        onChange={changeHandler}
+        name="confirmPassword"
+        value={confirmPassword}
+        placeholder="Confirm Password"
+      />
+      <Button>save</Button>
+      <p>password must contain at least 8 charachters</p>
+    </fieldset>
+  );
+};
+
+export default NewSettingsInfo;
--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import SettingsPage from "./views/SettingsPage"
+
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<SettingsPage />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
