156d56a551550b13181e11ec9f90d6c8cd910c4a
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-03-05 00:10:28 -0600

    created userSchema

---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    phone_num: {
+      type: Number,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    active: {
+      type: Boolean,
+      required: true
+    },
+    companies: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Company'
+      }
+    ]
+  },
+  { timestamps: true }
+);
