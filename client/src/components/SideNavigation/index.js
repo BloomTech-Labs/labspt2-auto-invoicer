c8c6062ef8c5fa2517b3b211c0b3382299b356eb
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-03-04 21:43:48 -0600

    testing

--- client/src/components/SideNavigation/index.js
+++ client/src/components/SideNavigation/index.js
@@ -1,30 +1,30 @@
-import React from "react";
+import React from 'react';
 // list of material ui components
-import Drawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 
 // react router dom methods
-import { NavLink, withRouter, Redirect } from "react-router-dom";
+import { NavLink, withRouter, Redirect } from 'react-router-dom';
 
 // list of icons used in the side navigation component
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Dehaze";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import HomeIcon from "@material-ui/icons/Home";
-import AboutIcon from "@material-ui/icons/Code";
-import BillingIcon from "@material-ui/icons/Payment";
-import SettingsIcon from "@material-ui/icons/Settings";
-import InvoicesIcon from "@material-ui/icons/Receipt";
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Dehaze';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import HomeIcon from '@material-ui/icons/Home';
+import AboutIcon from '@material-ui/icons/Code';
+import BillingIcon from '@material-ui/icons/Payment';
+import SettingsIcon from '@material-ui/icons/Settings';
+import InvoicesIcon from '@material-ui/icons/Receipt';
 
 // imported css here
-import "./SideNavigation.css";
+import './SideNavigation.css';
 
 // imported components here
-import AuthLanding from "../AuthLanding";
-import AuthSecured from "../AuthSecured";
+import AuthLanding from '../AuthLanding';
+import AuthSecured from '../AuthSecured';
 
 class SideNavigation extends React.Component {
   constructor(props) {
@@ -48,8 +48,8 @@ class SideNavigation extends React.Component {
     this.setState({ loggedIn: false, loggedOutClicked: true });
 
     /* ternary operator checking if token is available in local storage and deletes if it is */
-    return localStorage.getItem("jwt-auto-invoice")
-      ? localStorage.removeItem("jwt-auto-invoice")
+    return localStorage.getItem('jwt-auto-invoice')
+      ? localStorage.removeItem('jwt-auto-invoice')
       : null;
   };
 
@@ -61,16 +61,16 @@ class SideNavigation extends React.Component {
     // deconstruct state to get a list of needed attributes
     const { open, loggedIn, loggedOutClicked } = this.state;
     return (
-      <div className="navigation">
+      <div className='navigation'>
         {/* if log out is clicked it will redirect to landing */}
-        {loggedOutClicked ? <Redirect to="/" /> : null}
-        <header position="fixed" className="navbar">
+        {loggedOutClicked ? <Redirect to='/' /> : null}
+        <header position='fixed' className='navbar'>
           <IconButton
-            color="inherit"
-            aria-label="Open drawer"
+            color='inherit'
+            aria-label='Open drawer'
             onClick={this.handleDrawerOpen}
           >
-            <MenuIcon id="hamburger" />
+            <MenuIcon id='hamburger' />
           </IconButton>
           {/* if logged in show signup/signin else show */}
           {!loggedIn ? (
@@ -80,47 +80,47 @@ class SideNavigation extends React.Component {
           )}
         </header>
         <Drawer
-          anchor={document.body.clientWidth < 520 ? "top" : "left"}
+          anchor={document.body.clientWidth < 520 ? 'top' : 'left'}
           open={open}
         >
-          <div id="drawer-container">
+          <div id='drawer-container'>
             <IconButton onClick={this.handleDrawerOpen}>
-              <ChevronLeftIcon id="left-icon" />
+              <ChevronLeftIcon id='left-icon' />
             </IconButton>
           </div>
           <Divider />
           {/* if not logged in show public routes else show secured */}
           {!loggedIn ? (
-            <List className="all-icon-container">
+            <List className='all-icon-container'>
               {[
-                { title: "Home", icon: <HomeIcon /> },
-                { title: "About", icon: <AboutIcon /> }
+                { title: 'Home', icon: <HomeIcon /> },
+                { title: 'About', icon: <AboutIcon /> }
               ].map((text, index) => {
                 const { title, icon } = text;
                 return (
                   <NavLink
                     exact
-                    to={`${title}` === "Home" ? "/" : `/${title}`}
+                    to={`${title}` === 'Home' ? '/' : `/${title}`}
                     key={title}
-                    className="icon-container"
+                    className='icon-container'
                     onClick={() => {
                       this.setState({ open: !open });
                     }}
                   >
-                    <ListItem className="icon-item">
+                    <ListItem className='icon-item'>
                       <ListItemIcon>{icon}</ListItemIcon>
-                      <p className="icon-title">{title}</p>
+                      <p className='icon-title'>{title}</p>
                     </ListItem>
                   </NavLink>
                 );
               })}
             </List>
           ) : (
-            <List className="all-icon-container">
+            <List className='all-icon-container'>
               {[
-                { title: "Invoices", icon: <InvoicesIcon /> },
-                { title: "Billing", icon: <BillingIcon /> },
-                { title: "Settings", icon: <SettingsIcon /> }
+                { title: 'Invoices', icon: <InvoicesIcon /> },
+                { title: 'Billing', icon: <BillingIcon /> },
+                { title: 'Settings', icon: <SettingsIcon /> }
               ].map((text, index) => {
                 const { title, icon } = text;
                 return (
@@ -128,14 +128,14 @@ class SideNavigation extends React.Component {
                     exact
                     to={`/${title}`}
                     key={title}
-                    className="icon-container"
+                    className='icon-container'
                     onClick={() => {
                       this.setState({ open: !open });
                     }}
                   >
-                    <ListItem className="icon-item">
+                    <ListItem className='icon-item'>
                       <ListItemIcon>{icon}</ListItemIcon>
-                      <p className="icon-title">{title}</p>
+                      <p className='icon-title'>{title}</p>
                     </ListItem>
                   </NavLink>
                 );
