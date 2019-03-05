f7a7d36a8ec7a669e264c7b3e8360dae7da3b482
paulapivat <paul.apivat@gmail.com>
2019-03-05 12:24:19 +0700

    create invoice page adding

---
+++ client/src/components/reusableComponents/InvoiceItemTable/index.js
@@ -0,0 +1,120 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+import TextField from "@material-ui/core/TextField";
+
+const TAX_RATE = 0.07;
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 700
+  }
+});
+
+function ccyFormat(num) {
+  return `${num.toFixed(2)}`;
+}
+
+function priceRow(qty, unit) {
+  return qty * unit;
+}
+
+function createRow(id, desc, qty, unit) {
+  const price = priceRow(qty, unit);
+  return { id, desc, qty, unit, price };
+}
+
+function subtotal(items) {
+  return items.map(({ price }) => price).reduce((sum, i) => sum + i, 0);
+}
+
+const rows = [
+  ["Paperclips (Box)", 100, 1.15],
+  ["Paper (Stack)", 10, 45.99]
+  //   ["Waste Basket", 2, 17.99]
+].map((row, id) => createRow(id, ...row));
+
+const invoiceSubtotal = subtotal(rows);
+const invoiceTaxes = TAX_RATE * invoiceSubtotal;
+const invoiceTotal = invoiceTaxes + invoiceSubtotal;
+
+function index(props) {
+  const { classes } = props;
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Item</TableCell>
+            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="right">Rate</TableCell>
+            <TableCell align="right">Amount</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.id}>
+              <TableCell>{row.desc}</TableCell>
+              <TableCell align="right">{row.qty}</TableCell>
+              <TableCell align="right">{row.unit}</TableCell>
+              <TableCell align="right">{ccyFormat(row.price)}</TableCell>
+            </TableRow>
+          ))}
+
+          <TableRow>
+            <TableCell rowSpan={6} />
+            <TableCell colSpan={2}>Subtotal</TableCell>
+            <TableCell align="right">{ccyFormat(invoiceSubtotal)}</TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell colSpan={2}>Discount</TableCell>
+            <TableCell align="right">0%</TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell colSpan={2}>Tax</TableCell>
+            {/* <TableCell align="right">{`${(TAX_RATE * 100).toFixed(
+              0
+            )} %`}</TableCell> */}
+            <TableCell align="right">{ccyFormat(invoiceTaxes)}</TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell colSpan={2}>Shipping</TableCell>
+            <TableCell align="right">0</TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell colSpan={2}>Total</TableCell>
+            <TableCell align="right">{ccyFormat(invoiceTotal)}</TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell colSpan={2}>Amount Paid:</TableCell>
+            <TableCell align="right">$0</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+index.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(index);
---
+++ client/src/views/CreateInvoice/index.js
@@ -0,0 +1,72 @@
+// import packages
+import React, { Component } from "react";
+
+//import styles
+import "./CreateInvoice.css";
+
+//import components
+import AddressForm from "../../components/reusableComponents/AddressForm";
+import InvoiceItemTable from "../../components/reusableComponents/InvoiceItemTable";
+import AddCircle from "@material-ui/icons/AddCircle";
+////
+
+export default class index extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        {" "}
+        <div className="top-nav">
+          <div className="top-nav-left">
+            <ul className="breadcrumb">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Invoices</a>
+              </li>
+              <li>
+                <a href="#">New</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="top-nav-right">
+            <div className="credits">Credits: 3</div>
+            <div className="signout-button">Sign Out</div>
+          </div>
+        </div>
+        <div className="mid-container">
+          <div className="sidebar-nav">
+            <div className="sidebar-nav-button">Invoice</div>
+            <div className="sidebar-nav-button">Billing</div>
+            <div className="sidebar-nav-button">Settings</div>
+          </div>
+          <div className="invoice-list-container">
+            <div className="invoice-details">
+              <div className="invoice-logo">
+                Add Your Company Logo: <AddCircle />
+              </div>
+              <div className="invoice-number-container">
+                <div className="invoice-number-item">Invoice Number</div>
+                <div className="invoice-number-item">Date</div>
+                <div className="invoice-number-item">Due Date</div>
+                <div className="invoice-number-item">Balance Due: $0.00</div>
+              </div>
+            </div>
+            <div className="invoice-address">
+              <AddressForm />
+            </div>
+
+            <div className="item-details">
+              <InvoiceItemTable />
+            </div>
+          </div>
+        </div>
+        <div className="footer">Footer</div>
+      </div>
+    );
+  }
+}
+
+// Add functionality to form
+// submit handler, save responses to local state
