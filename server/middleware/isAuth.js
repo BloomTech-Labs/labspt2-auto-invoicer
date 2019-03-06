dc5c83f9572394d693fd769fe75327371ca2acef
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-03-06 12:54:46 -0500

    corrected auth middleware to block any incoming request if not authorized

--- server/middleware/isAuth.js
+++ server/middleware/isAuth.js
@@ -5,24 +5,20 @@ const secret = process.env.SECRET || 'testingJWT';
 module.exports = (req, res, next) => {
   const authHeader = req.get('Authorization')
   if (!authHeader) {
-    req.isAuth = false;
-    return next();
+    throw new Error('you are not logged in, please log in')
   }
   const token = authHeader.split(' ')[1];
   if (!token || token === '') {
-    req.isAuth = false;
-    return next()
+    throw new Error('you are not logged in, please log in')
   }
   try {
     decodedToken = jwt.verify(token, secret)
   } catch (error) {
-    req.isAuth = false;
-    return next()
+    throw new Error('unable to verify session at this moment, please try again later')
   }
 
   if (!decodedToken) {
-    req.isAuth = false;
-    return next()
+    throw new Error('your session has expired, please log back in')
   }
   req.isAuth = true;
   req.userId = decodedToken.userId
