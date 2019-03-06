5940435a40775c1cf19188f6e12aa53adb4ee759
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-03-06 14:14:36 -0500

    corrected another naming issue

--- server/graphql/schema/index.js
+++ server/graphql/schema/index.js
@@ -32,39 +32,6 @@ input UserInput {
   phone_num: String!
 }
 
-type RootQuery {
-  users: [User!]!
-  companies: [Company!]!
-}
-
-type RootMutation {
-  createUser(userInput: UserInput): User
-}
-
-schema {
-  query: RootQuery
-  mutation: RootMutation
-}
-type Company {
-  _id: ID!
-  name: String!
-  email: String!
-  phone_num: String!
-  address_1: String!
-  address_2: String
-  city: String!
-  state: String!
-  postal_code: Int!
-  unlimited_tier: Boolean!
-  credits: Int!
-  users: [User!]!
-}
-input UserInput {
-  email: String!
-  password: String!
-  name: String!
-  phone_num: String!
-}
 type AuthData {
   userId: ID!
   token: String!
@@ -76,20 +43,13 @@ type RootQuery {
   companies: [Company!]!
   login(email: String!, password: String!): AuthData!
 }
+
 type RootMutation {
   createUser(userInput: UserInput): User
 }
+
 schema {
   query: RootQuery
   mutation: RootMutation
-}
-
-input UserInput {
-  email: String!
-  password: String!
-}
-
-type RootMutation {
-  createUser(userInput: UserInput): User
 }
 `);
