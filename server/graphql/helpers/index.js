3efc86e2407e2f6579024b1c7bfccb5696499c00
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-03-07 16:23:05 -0600

    finished reusable helper function to query any document by any field

--- server/graphql/helpers/index.js
+++ server/graphql/helpers/index.js
@@ -22,4 +22,34 @@ const updateDocumentById = async (documentInput, id, Model) => {
   }
 };
 
-module.exports = { updateDocumentById };
+const findDocumentsByAnyField = async (documentInput, fields, Model) => {
+  try {
+    Object.keys(documentInput).forEach(key => {
+      if (!documentInput[key]) {
+        delete documentInput[key];
+      }
+    });
+    const value = Object.values(documentInput)[0];
+    const arr = fields.map(field => {
+      if (field.type === typeof value) {
+        return { [field.name]: value };
+      }
+    });
+    const newArr = [];
+    arr.map(item => {
+      if (item) {
+        newArr.push(item);
+      }
+    });
+    const documents = await Model.find({
+      $or: newArr
+    });
+    return documents.map(document => {
+      return { ...document._doc };
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = { updateDocumentById, findDocumentsByAnyField };
