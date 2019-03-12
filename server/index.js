11a438eb3cefffed07d6aeddf96403389f859136
Joseph Thompson <joe.thompson838@gmail.com>
2019-03-12 19:13:42 -0400

    added a pdf creator for invoices

--- server/index.js
+++ server/index.js
@@ -1,40 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const graphqlHttp = require('express-graphql');
-const { connect } = require('mongoose');
-const serverless = require('serverless-http');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-
-const authRouter = require('./auth');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const graphqlHttp = require("express-graphql");
+const { connect } = require("mongoose");
+const serverless = require("serverless-http");
 
 const app = express();
 const PORT = process.env.APP_PORT || 5000;
 
-const GraphQLSchema = require('./graphql/schema');
-const GraphQLResolvers = require('./graphql/resolvers');
-const authorize = require('./middleware/isAuth');
+const GraphQLSchema = require("./graphql/schema");
+const GraphQLResolvers = require("./graphql/resolvers");
+const authorize = require("./middleware/isAuth");
+
+// for pdf creation
+const pdf = require("html-pdf");
+const pdfTemplate = require("./documents");
 
 app.use(express.json(), cors(), helmet());
 // app.use(authorize)
 
 app.use(
-  cookieSession({
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: process.env.COOKIE_SESSION_KEY
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/auth', authRouter);
-
-app.use(
-  '/graphql',
+  "/graphql",
   graphqlHttp({
     schema: GraphQLSchema,
     rootValue: GraphQLResolvers,
@@ -42,11 +30,25 @@ app.use(
   })
 );
 
+// add a route for pdf creation
+app.post("/create-pdf", (req, res) => {
+  const file = req.body;
+  pdf.create(pdfTemplate(file), {}).toFile("documents/result.pdf", err => {
+    if (err) {
+      res.send(Promise.reject());
+    } else res.send(Promise.resolve());
+  });
+});
+
+// add a route for generating pdf for client
+app.get("/fetch-pdf", (req, res) => {
+  res.sendFile(`${__dirname}/documents/result.pdf`);
+});
+
 connect(
   `mongodb+srv://${process.env.DB_USER}:${
     process.env.DB_PASSWORD
-  }@autoinvoice-evkdc.mongodb.net/${process.env.DB_NAME}?retryWrites=true`,
-  { useNewUrlParser: true }
+  }@autoinvoice-evkdc.mongodb.net/${process.env.DB_NAME}?retryWrites=true`
 )
   .then(app.listen(PORT, console.log(`App is up and running on port ${PORT}!`)))
   .catch(err => console.log(err));
