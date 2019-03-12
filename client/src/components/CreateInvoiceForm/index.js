473d5c4a725128e3bb3bfd6bf035410193c187e9
Joseph Thompson <joe.thompson838@gmail.com>
2019-03-12 19:13:42 -0400

    added a pdf creator for invoices

--- client/src/components/CreateInvoiceForm/index.js
+++ client/src/components/CreateInvoiceForm/index.js
@@ -216,6 +216,7 @@ export default class index extends Component {
     };
 
     console.log("Invoice Data Object:", formPayload);
+    this.props.click(formPayload);
     this.handleClearForm(e);
   }
 
