1279268867e14c0d5d8e23c500f95c748b323903
Joseph Thompson <joe.thompson838@gmail.com>
2019-03-12 19:13:42 -0400

    added a pdf creator for invoices

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require('express');
 const cors = require('cors');
@@ -14,33 +14,40 @@ const authRouter = require('./auth');
 const app = express();
 const PORT = process.env.APP_PORT || 5000;
 
-const GraphQLSchema = require('./graphql/schema');
-const GraphQLResolvers = require('./graphql/resolvers');
-const authorize = require('./middleware/isAuth');
+const GraphQLSchema = require("./graphql/schema");
+const GraphQLResolvers = require("./graphql/resolvers");
+const authorize = require("./middleware/isAuth");
+
+// for pdf creation
+const pdf = require("html-pdf");
+const pdfTemplate = require("./documents");
 
 app.use(express.json(), cors(), helmet());
 // app.use(authorize)
 
 app.use(
-  cookieSession({
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: process.env.COOKIE_SESSION_KEY
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/auth', authRouter);
-
-app.use(
-  '/graphql',
+  "/graphql",
   graphqlHttp({
     schema: GraphQLSchema,
     rootValue: GraphQLResolvers,
     graphiql: true
   })
 );
+
+// add a route for pdf creation
+app.post("/create-pdf", (req, res) => {
+  const file = req.body;
+  pdf.create(pdfTemplate(file), {}).toFile("documents/result.pdf", err => {
+    if (err) {
+      res.send(Promise.reject());
+    } else res.send(Promise.resolve());
+  });
+});
+
+// add a route for generating pdf for client
+app.get("/fetch-pdf", (req, res) => {
+  res.sendFile(`${__dirname}/documents/result.pdf`);
+});
 
 connect(
   `mongodb+srv://${process.env.DB_USER}:${
