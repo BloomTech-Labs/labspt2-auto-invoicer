103f8e721c789bc2fb5f124bae59c7a4f48ab7a3
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-03-16 15:45:03 -0400

    corrected the fields in the Customer schema

--- server/models/customer.js
+++ server/models/customer.js
@@ -5,15 +5,39 @@ const customerSchema = new Schema({
     type: String,
     required: true
   },
-  address: {
-    type: String,
-    required: true
-  },
   email: {
     type: String,
     required: true
   },
   phone_num: {
+    type: String,
+    required: true
+  },
+  country_code: {
+    type: String,
+    required: true
+  },
+  address_1: {
+    type: String,
+    required: true
+  },
+  address_2: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  postal_code: {
+    type: String,
+    required: true
+  },
+  country:  {
     type: String,
     required: true
   },
