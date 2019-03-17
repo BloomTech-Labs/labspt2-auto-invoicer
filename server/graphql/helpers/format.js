650ee248fec5b3bade97650e36a2daea7ffbf96e
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-03-17 17:32:04 -0400

    removed the country code check

--- server/graphql/helpers/format.js
+++ server/graphql/helpers/format.js
@@ -6,9 +6,7 @@ const formatData = document => {
   });
   if (document.phone_num) {
     const regx = /\D+/g;
-    const formatted = document.phone_num.replace(regx, '');
-    document.phone_num =
-      formatted.charAt(0) === '1' ? formatted.slice(1) : formatted;
+    document.phone_num = document.phone_num.replace(regx, '');
   }
   return document;
 };
