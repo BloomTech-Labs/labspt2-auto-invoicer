f44186a6de7f240b70edcf626b3d0a917c8b1cc4
Dewayne87 <dewaynelindsay87@gmail.com>
2019-03-18 19:54:19 -0700

    added basic snapshots of all landing pages to being testing and added jest-dom to package.json

--- client/src/components/LandingText/LandingText.test.js
+++ client/src/components/LandingText/LandingText.test.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { render } from "react-testing-library";
-
+import "jest-dom/extend-expect";
 import LandingText from "../LandingText";
 
 describe("<LandingText />", () => {
   it("renders Landing Text", () => {
     render(<LandingText />);
-  });
+  })
+  it('renders', () => {
+    const { asFragment } = render(<LandingText />);
+    expect(asFragment()).toMatchSnapshot();
+  })
 });
