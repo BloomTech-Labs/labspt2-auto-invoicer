2bb9fc69fb19e196ddefe2397688f0e06990e492
paulapivat <paul.apivat@gmail.com>
2019-03-20 20:45:06 +0700

    add netlify-deploy address to Authorized Javascript origins for Google Cal Api

--- client/src/components/GoogleCalApi/constants.js
+++ client/src/components/GoogleCalApi/constants.js
@@ -1,3 +1,5 @@
+//GoogleCalAPI project name: Invoice OAuth
+//Add https://deploy-preview-61--auto-invoicer.netlify.com/ to Authorized Javascript Origins
 export const CLIENT_ID =
   "959539137198-ehp7h5qhg3ntn9nldvn607n3n0s6t01g.apps.googleusercontent.com";
 export const DISCOVERY_DOCS = [
