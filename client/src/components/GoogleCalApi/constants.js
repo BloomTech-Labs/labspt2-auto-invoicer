eeaedfb7dd294b40ee2e2e5779a26578f44dbbcb
paulapivat <paul.apivat@gmail.com>
2019-03-20 16:55:34 +0700

    add two directories AddToCalendar and GoogleCalApi to components

---
+++ client/src/components/GoogleCalApi/constants.js
@@ -0,0 +1,6 @@
+export const CLIENT_ID =
+  "959539137198-ehp7h5qhg3ntn9nldvn607n3n0s6t01g.apps.googleusercontent.com";
+export const DISCOVERY_DOCS = [
+  "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
+];
+export const SCOPES = "https://www.googleapis.com/auth/calendar";
