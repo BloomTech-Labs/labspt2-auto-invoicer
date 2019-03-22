b65bd583587a4940f79f6c915c2b20a59cb59441
Andres Rivera <andres.rivera.toro@gmail.com>
2019-03-22 02:35:10 -0400

    Fix Stripe payment endpoint

--- server/stripe/index.js
+++ server/stripe/index.js
@@ -1,26 +1,18 @@
 const router = require('express').Router();
 const stripe = require('stripe')(process.env.STRIPE_CHECKOUT_SECRET_KEY);
 
-router.post('/charge', (req, res) => {
-  const { amount, currency, stripeToken, name, quantity } = req.body;
-  stripe.charges.create(
-    {
-      amount: amount * 100,
-      currency,
-      source: stripeToken,
-      description: 'Testing charges'
-    },
-    (error, charge) => {
-      try {
-        res.json({
-          message: `You successfully bought ${quantity} units of ${name}!`
-        });
-        res.redirect('/success');
-      } catch (err) {
-        throw new Error(error.message);
-      }
-    }
-  );
+router.post('/charge', async (req, res) => {
+  try {
+    const { status } = await stripe.charges.create({
+      amount: 1000,
+      currency: 'usd',
+      description: 'Testing charges',
+      source: req.body
+    });
+    res.json({ status });
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
 module.exports = router;
