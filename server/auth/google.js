e67ac3f118eed2f149650ab1c3cb3a5330940b59
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-03-29 00:19:31 -0500

    fixed typo

--- server/auth/google.js
+++ server/auth/google.js
@@ -6,11 +6,7 @@ const User = require('../models/user');
 passport.use(
   new GoogleStrategy(
     {
-<<<<<<< HEAD
-      callbackURL: '/',
-=======
       callbackURL: '/dev/auth/google/home',
->>>>>>> 79526c93a311afb6df57caa3f4f34c96c8811301
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     },
--- server/stripe/index.js
+++ server/stripe/index.js
@@ -3,15 +3,18 @@ const stripe = require('stripe')(process.env.STRIPE_CHECKOUT_SECRET_KEY);
 
 router.post('/charge', async (req, res) => {
   try {
-    const { status } = await stripe.charges.create({
-      amount: 1000,
-      currency: 'usd',
-      description: 'Testing charges',
-      source: req.body
+    const { stripeToken, currency, quantity } = JSON.parse(req.body);
+    const status = await stripe.charges.create({
+      amount: quantity ? quantity * 990 : 990,
+      currency,
+      description: `Purchase of ${
+        quantity ? quantity + 'credits' : '1 Month Unlimited Plan!'
+      }`,
+      source: stripeToken
     });
     res.json({ status });
   } catch (err) {
-    throw new Error(err);
+    throw err;
   }
 });
 
