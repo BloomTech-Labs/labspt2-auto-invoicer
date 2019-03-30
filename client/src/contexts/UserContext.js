b7fd5e546ffbdeaa86bf4403249cdf6c3d963d07
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-03-30 18:19:18 -0400

    refactored both contexts

--- client/src/contexts/UserContext.js
+++ client/src/contexts/UserContext.js
@@ -1,36 +1,42 @@
 import React from 'react'
 
 import {FetchUser} from '../graphQL/queries/users'
+
 export const UserContext = React.createContext()
 
 export class UserProvider extends React.Component {
-  
-  state = {
-    userID: '',
-    email: '',
-    name: '',
-    phone_num: '',
-    companies: [],
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      userID: '',
+      email: '',
+      name: '',
+      phone_num: '',
+      companies: [],
+      defaultCompany: ''
+    };
+
+    this.fetchUser = async (userID) => {
+      const returnedData = '_id name email phone_num companies {_id name}'
+      const result = await FetchUser(userID, returnedData)
+      const {_id, email, name, phone_num, companies} = result.user
+      this.setState({
+        userID: _id,
+        email,
+        name, phone_num, 
+        companies
+      })
   }
-  //hardcoded userID, once we have the JWT set up, then we can grab
-  //and pass in the userID to FetchUser()
-  async componentDidMount() {
-    // const returned = '_id name email phone_num companies {_id name}'
-    // const result = await FetchUser("5c8d860b7fef7140f485950c", returned)
-    // const {user} = result
-    // this.setState({
-    //   userID: user._id,
-    //   email: user.email,
-    //   name: user.name,
-    //   phone_num: user.phone_num,
-    //   companies: user.companies
-    // })
+  
   }
 
   render() {
+    const {fetchUser} = this;
+    const userState = this.state
     return (
       <UserContext.Provider value={
-        {...this.state}
+        {userState, fetchUser}
         }>
         {this.props.children}
       </UserContext.Provider>
@@ -38,3 +44,4 @@ export class UserProvider extends React.Component {
   }
 }
 
+export const UserConsumer = UserContext.Consumer;
