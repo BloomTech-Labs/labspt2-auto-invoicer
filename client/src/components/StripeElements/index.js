7b17e312b47196a725cdc007345481f8384bd7aa
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-04-07 21:44:33 -0400

    credits reflects new number when more is purchased or one is used

--- client/src/components/StripeElements/index.js
+++ client/src/components/StripeElements/index.js
@@ -4,6 +4,7 @@ import StripeCheckoutForm from '../StripeCheckoutForm';
 
 import Typography from '@material-ui/core/Typography';
 import Slide from '@material-ui/core/Slide';
+import { CompanyConsumer } from '../../contexts/CompanyContext';
 
 class StripeElements extends Component {
   state = { checked: false };
@@ -12,27 +13,35 @@ class StripeElements extends Component {
   }
   render() {
     return (
-      <StripeProvider apiKey="pk_test_aQUyLjBzj0vLD5DfVQv55rFk">
-        <div>
-          <Slide
-            direction="down"
-            in={this.state.checked}
-            mountOnEnter
-            unmountOnExit
-          >
-            <Typography
-              style={{ fontSize: '4rem', marginBottom: 40 }}
-              variant="h3"
-              gutterBottom
-            >
-              Purchase your plan
-            </Typography>
-          </Slide>
-          <Elements>
-            <StripeCheckoutForm />
-          </Elements>
-        </div>
-      </StripeProvider>
+      <CompanyConsumer>
+        {({fetchPlanOrCredits}) => {
+          return (
+            <StripeProvider apiKey="pk_test_aQUyLjBzj0vLD5DfVQv55rFk">
+              <div>
+                <Slide
+                  direction="down"
+                  in={this.state.checked}
+                  mountOnEnter
+                  unmountOnExit
+                >
+                  <Typography
+                    style={{ fontSize: '4rem', marginBottom: 40 }}
+                    variant="h3"
+                    gutterBottom
+                  >
+                    Purchase your plan
+                  </Typography>
+                </Slide>
+                <Elements>
+                  <StripeCheckoutForm 
+                  fetchPlanOrCredits={fetchPlanOrCredits}/>
+                </Elements>
+              </div>
+            </StripeProvider>
+
+          )
+        }}
+      </CompanyConsumer>
     );
   }
 }
