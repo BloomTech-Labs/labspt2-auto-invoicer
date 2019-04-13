5d4cd238c083635fef03a6640dbeeb4092854ea2
paulapivat <paul.apivat@gmail.com>
2019-04-14 02:35:31 +0700

    edit index.js component

--- client/src/components/CreateInvoiceForm2/index.js
+++ client/src/components/CreateInvoiceForm2/index.js
@@ -48,12 +48,67 @@ const StyledInvoiceBalance = styled.section`
 `;
 
 class CreateInvoiceForm2 extends Component {
-  constructor() {
-    super();
-    this.state = {
-      invoiceItems: [{ item: "", quantity: "", rate: "", amount: "" }]
-    };
-  }
+  state = {
+    invoiceNumber: "",
+    invoiceDescription: "",
+    selectedDate: new Date(),
+    invoiceDueDate: new Date(),
+    billToItems: [
+      { address1: "", address2: "", city: "", state: "", zip: "", email: "" }
+    ],
+    invoiceItems: [{ item: "", quantity: "", rate: "", amount: "" }],
+    invoiceBalanceItems: [
+      {
+        subtotal: "",
+        discount: "",
+        tax: "",
+        shipping: "",
+        total: "",
+        amountPaid: ""
+      }
+    ],
+    invoiceNotesTermsItems: [{ notes: "", terms: "" }]
+  };
+
+  handleInputChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  handleDateChange = date => {
+    this.setState({ selectedDate: date });
+  };
+
+  handleInvoiceDueDateChange = date => {
+    this.setState({ invoiceDueDate: date });
+  };
+
+  handleBillToItemsChange = event => {
+    if (
+      ["address1", "address2", "city", "state", "zip", "email"].includes(
+        event.target.className
+      )
+    ) {
+      const billToItems = [...this.state.billToItems];
+      billToItems[event.target.dataset.id][
+        event.target.className
+      ] = event.target.value.toUpperCase();
+      this.setState({ billToItems }, () => console.log("Bill To Items"));
+    } else {
+      this.setState({ [event.target.name]: event.target.value.toUpperCase() });
+    }
+  };
+
+  handleInvoiceItemsInputChange = e => {
+    if (["item", "quantity", "rate", "amount"].includes(e.target.className)) {
+      const invoiceItems = [...this.state.invoiceItems];
+      invoiceItems[e.target.dataset.id][
+        e.target.className
+      ] = e.target.value.toUpperCase();
+      this.setState({ invoiceItems }, () => console.log("Invoice Items"));
+    } else {
+      this.setState({ [e.target.name]: e.target.value.toUpperCase() });
+    }
+  };
 
   addInvoiceItem = e => {
     e.preventDefault();
@@ -65,26 +120,92 @@ class CreateInvoiceForm2 extends Component {
     }));
   };
 
+  handleInvoiceBalanceItemsChange = e => {
+    if (
+      [
+        "subtotal",
+        "discount",
+        "tax",
+        "shipping",
+        "total",
+        "amountPaid"
+      ].includes(e.target.className)
+    ) {
+      const invoiceBalanceItems = [...this.state.invoiceBalanceItems];
+      invoiceBalanceItems[e.target.dataset.id][
+        e.target.className
+      ] = e.target.value.toUpperCase();
+      this.setState({ invoiceBalanceItems }, () =>
+        console.log("Invoice Balance Items")
+      );
+    } else {
+      this.setState({ [e.target.name]: e.target.value.toUpperCase() });
+    }
+  };
+
+  handleInvoiceNotesTermsItemsChange = e => {
+    if (["notes", "terms"].includes(e.target.className)) {
+      const invoiceNotesTermsItems = [...this.state.invoiceNotesTermsItems];
+      invoiceNotesTermsItems[e.target.dataset.id][
+        e.target.className
+      ] = e.target.value.toUpperCase();
+      this.setState({ invoiceNotesTermsItems }, () =>
+        console.log("Invoice Notes & Terms items")
+      );
+    } else {
+      this.setState({ [e.target.name]: e.target.value.toUpperCase() });
+    }
+  };
+
+  // handleClearForm = e => {
+  //   e.preventDefault();
+  //   this.setState({
+  //     invoiceNumber: "",
+  //     invoiceDescription: ""
+  //   });
+  // };
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      // invoiceNumber: "",
+      // invoiceDescription: ""
+    });
+    console.log(this.state);
+  };
+
   render() {
     const { classes } = this.props;
     return (
-      <Grid container spacing={12}>
+      <Grid container spacing={16}>
         <Paper className={classes.paper}>
-          <form className={classes.container}>
+          <div className={classes.container}>
             <StyledSection>
               <Grid item xs={6}>
-                <InvoiceNumberInput />
+                <InvoiceNumberInput
+                  onChangeHandler={this.handleInputChange("invoiceNumber")}
+                  value={this.state.invoiceNumber}
+                />
               </Grid>
               <Grid item xs={6}>
-                <DateIssue />
+                <DateIssue
+                  onChangeHandler={this.handleDateChange}
+                  value={this.state.selectedDate}
+                />
               </Grid>
               <Grid item xs={3}>
-                <DueDate />
+                <DueDate
+                  onChangeHandler={this.handleInvoiceDueDateChange}
+                  value={this.state.invoiceDueDate}
+                />
               </Grid>
             </StyledSection>
             <StyledSection>
               <Grid item xs={9}>
-                <InvoiceDescription />
+                <InvoiceDescription
+                  onChangeHandler={this.handleInputChange("invoiceDescription")}
+                  value={this.state.invoiceDescription}
+                />
               </Grid>
               <Grid item xs={3}>
                 <UploadLogo />
@@ -92,7 +213,12 @@ class CreateInvoiceForm2 extends Component {
             </StyledSection>
             <StyledAddress>
               <Grid item xs={4}>
-                <BillTo />
+                <form
+                  onSubmit={this.handleFormSubmit}
+                  onChange={this.handleBillToItemsChange}
+                >
+                  <BillTo billToItems={this.state.billToItems} />
+                </form>
               </Grid>
             </StyledAddress>
             <StyledInvoiceItem>
@@ -115,16 +241,34 @@ class CreateInvoiceForm2 extends Component {
             </StyledInvoiceItem>
             <StyledInvoiceBalance>
               <Grid item xs={4}>
-                <InvoiceNotesTerms />
+                <form
+                  onSubmit={this.handleFormSubmit}
+                  onChange={this.handleInvoiceNotesTermsItemsChange}
+                >
+                  <InvoiceNotesTerms
+                    invoiceNotesTermsItems={this.state.invoiceNotesTermsItems}
+                  />
+                </form>
               </Grid>
               <Grid item xs={4}>
-                <InvoiceBalance />
+                <form
+                  onSubmit={this.handleFormSubmit}
+                  onChange={this.handleInvoiceBalanceItemsChange}
+                >
+                  <InvoiceBalance
+                    invoiceBalanceItems={this.state.invoiceBalanceItems}
+                  />
+                </form>
               </Grid>
             </StyledInvoiceBalance>
-          </form>
-          <Button variant="contained" color="primary">
-            Generate
-          </Button>
+            <Button
+              onClick={this.handleFormSubmit}
+              variant="contained"
+              color="primary"
+            >
+              Generate
+            </Button>
+          </div>
         </Paper>
       </Grid>
     );
