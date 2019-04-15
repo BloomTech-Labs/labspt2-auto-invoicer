4efe554cab4b31837746df8f85ad72bea5876ae6
paulapivat <paul.apivat@gmail.com>
2019-04-15 21:51:15 +0700

    edit invoicedescription component

--- client/src/components/CreateInvoiceForm2/InvoiceDescription.js
+++ client/src/components/CreateInvoiceForm2/InvoiceDescription.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-//import { withStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 
-//import styles from "./styles";
-import { TextField } from "../../../node_modules/@material-ui/core";
+import styles from "./styles";
+//import { TextField } from "../../../node_modules/@material-ui/core";
+import { TextField } from "@material-ui/core";
 
 const InvoiceDescription = props => {
   const { classes, onChangeHandler, value } = props;
@@ -11,9 +12,9 @@ const InvoiceDescription = props => {
     <TextField
       id="filled-name"
       label="Invoice Description"
-      //className={classes.textField}
-      //value={value}
-      //onChange={onChangeHandler}
+      className={classes.textField}
+      value={value}
+      onChange={onChangeHandler}
       style={{ width: 400 }}
       InputLabelProps={{ style: { fontSize: 20 } }}
       InputProps={{ style: { fontSize: 20 } }}
@@ -23,4 +24,4 @@ const InvoiceDescription = props => {
   );
 };
 
-export default InvoiceDescription;
+export default withStyles(styles)(InvoiceDescription);
