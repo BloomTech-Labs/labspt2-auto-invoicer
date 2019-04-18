38a80043b08e3ab19510f4f02342ebfc89998391
paulapivat <paul.apivat@gmail.com>
2019-04-18 08:25:43 +0700

    make edits to Invoice Model

--- server/models/invoice.js
+++ server/models/invoice.js
@@ -1,39 +1,11 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const invoiceSchema = new Schema({
   invoiceNumber: {
     type: String,
     required: true
   },
-  languageSelection: {
-    type: String,
-    required: true
-  },
-  currencySelection: {
-    type: String,
-    required: true
-  },
-  addressFrom: {
-    type: String,
-    required: true
-  },
-  addressTo: {
-    type: String,
-    required: true
-  },
-  cityTo: {
-    type: String,
-    required: false
-  },
-  stateRegionTo: {
-    type: String,
-    required: false
-  },
-  zipCodeTo: {
-    type: String,
-    required: true
-  },
-  clientEmailTo: {
+  invoiceDescription: {
     type: String,
     required: true
   },
@@ -45,7 +17,28 @@ const invoiceSchema = new Schema({
     type: String,
     required: true
   },
-  balanceDue: {
+  company: {
+    type: String,
+    required: true
+  },
+
+  cityTo: {
+    type: String,
+    required: false
+  },
+  stateTo: {
+    type: String,
+    required: false
+  },
+  zipCodeTo: {
+    type: String,
+    required: true
+  },
+  addressTo: {
+    type: String,
+    required: true
+  },
+  emailTo: {
     type: String,
     required: true
   },
@@ -73,14 +66,21 @@ const invoiceSchema = new Schema({
     type: String,
     required: true
   },
-  invoiceNotes: {
+  balanceDue: {
     type: String,
     required: true
   },
-  invoiceTerms: {
+  //Notes & Terms
+  notes: {
     type: String,
     required: true
   },
+  terms: {
+    type: String,
+    required: true
+  },
+  // invoiceItems
+  // invoiceItems: [{ item: "", quantity: "", rate: "", amount: "" }],
   paid: {
     type: Boolean,
     default: false
@@ -105,8 +105,116 @@ const invoiceSchema = new Schema({
     type: String,
     required: true
   }
-
-  // invoiceItems: [{...}]
-  // (not required) subtotal, tax, total, invoiceNotes, invoiceTerms
 });
-module.exports = model('Invoice', invoiceSchema);
+
+// const invoiceSchema = new Schema({
+//   invoiceNumber: {
+//     type: String,
+//     required: true
+//   },
+//   languageSelection: {
+//     type: String,
+//     required: true
+//   },
+//   currencySelection: {
+//     type: String,
+//     required: true
+//   },
+//   addressFrom: {
+//     type: String,
+//     required: true
+//   },
+//   addressTo: {
+//     type: String,
+//     required: true
+//   },
+//   cityTo: {
+//     type: String,
+//     required: false
+//   },
+//   stateRegionTo: {
+//     type: String,
+//     required: false
+//   },
+//   zipCodeTo: {
+//     type: String,
+//     required: true
+//   },
+//   clientEmailTo: {
+//     type: String,
+//     required: true
+//   },
+//   selectedDate: {
+//     type: String,
+//     required: true
+//   },
+//   invoiceDueDate: {
+//     type: String,
+//     required: true
+//   },
+//   balanceDue: {
+//     type: String,
+//     required: true
+//   },
+//   subtotal: {
+//     type: String,
+//     required: true
+//   },
+//   discount: {
+//     type: String,
+//     required: true
+//   },
+//   tax: {
+//     type: String,
+//     required: true
+//   },
+//   shipping: {
+//     type: String,
+//     required: true
+//   },
+//   total: {
+//     type: String,
+//     required: true
+//   },
+//   amountPaid: {
+//     type: String,
+//     required: true
+//   },
+//   invoiceNotes: {
+//     type: String,
+//     required: true
+//   },
+//   invoiceTerms: {
+//     type: String,
+//     required: true
+//   },
+//   paid: {
+//     type: Boolean,
+//     default: false
+//   },
+//   companyID: {
+//     type: String,
+//     required: true
+//   },
+//   companyName: {
+//     type: String,
+//     required: true
+//   },
+//   userID: {
+//     type: String,
+//     required: true
+//   },
+//   userName: {
+//     type: String,
+//     required: true
+//   },
+//   customerID: {
+//     type: String,
+//     required: true
+//   }
+
+//   // invoiceItems: [{...}]
+//   // (not required) subtotal, tax, total, invoiceNotes, invoiceTerms
+// });
+
+module.exports = model("Invoice", invoiceSchema);
