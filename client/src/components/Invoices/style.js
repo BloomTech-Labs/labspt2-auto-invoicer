99e92e90a567ba4aae6d94389ebbbb5611c81cf7
Dewayne87 <dewaynelindsay87@gmail.com>
2019-04-18 19:19:23 -0700

    added create button to desktop view and pop up button from menu icon

--- client/src/components/Invoices/style.js
+++ client/src/components/Invoices/style.js
@@ -1,8 +1,11 @@
 import { fade } from "@material-ui/core/styles/colorManipulator";
 export default theme => ({
     root: {
-        width: "100%",
+        width: "99%",
         marginTop: theme.spacing.unit * 3
+    },
+    rootbar:{
+        width:"100%",
     },
     table: {
         minWidth: 1000
@@ -15,53 +18,64 @@ export default theme => ({
     },
     menuButton: {
         marginLeft: -12,
-        marginRight: 20
+        marginRight: 20,
     },
     title: {
-        display: "none",
-        [theme.breakpoints.up("sm")]: {
-            display: "block"
-        }
+        fontSize: 30,
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+            fontSize: 50,
+        },
     },
     search: {
-        position: "relative",
+        position: 'relative',
         borderRadius: theme.shape.borderRadius,
         backgroundColor: fade(theme.palette.common.white, 0.15),
-        "&:hover": {
-            backgroundColor: fade(theme.palette.common.white, 0.25)
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
         },
-        marginLeft: 0,
-        width: "50%",
-        [theme.breakpoints.up("sm")]: {
-            marginLeft: theme.spacing.unit,
-            width: "30%"
-        }
+        marginRight: theme.spacing.unit * 2,
+        marginLeft: 30,
+        width: '43%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: 120,
+            width: '40%',
+        },
     },
     searchIcon: {
-        width: theme.spacing.unit * 9,
-        height: "100%",
-        position: "absolute",
-        pointerEvents: "none",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center"
+        width: theme.spacing.unit * 11,
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     inputRoot: {
-        color: "inherit",
-        width: "100%"
+        color: 'inherit',
+        width: '100%',
     },
     inputInput: {
         paddingTop: theme.spacing.unit,
         paddingRight: theme.spacing.unit,
         paddingBottom: theme.spacing.unit,
         paddingLeft: theme.spacing.unit * 10,
-        transition: theme.transitions.create("width"),
-        width: "100%",
-        [theme.breakpoints.up("sm")]: {
-            width: 120,
-            "&:focus": {
-                width: 200
-            }
-        }
-    }
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: 300,
+        },
+    },
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+        },
+    },
+    sectionMobile: {
+        display: 'flex',
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
+    },
 });
