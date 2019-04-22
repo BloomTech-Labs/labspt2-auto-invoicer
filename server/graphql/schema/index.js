9337a3533eafbcc75bedc317325fc0abf36ca279
Andres Rivera <andres.rivera.toro@gmail.com>
2019-04-22 01:42:20 -0400

    Update GraphQL schema

--- server/graphql/schema/index.js
+++ server/graphql/schema/index.js
@@ -1,236 +1,213 @@
-const { buildSchema } = require("graphql");
+const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-type User {
-  _id: ID!
-  email: String!
-  name: String!
-  phone_num: String
-  companies: [Company!]
-  invoices: [Invoice!]
-  googleId: String
-  facebookId: String
-}
+  type User {
+    _id: ID!
+    email: String!
+    name: String!
+    password: String
+    phoneNumber: String
+    googleId: String
+    facebookId: String
+    companies: [Company!]
+    invoices: [Invoice!]
+    defaultCompany: String
+  }
 
-type Company {
-  _id: ID!
-  name: String!
-  email: String!
-  phone_num: String!
-  country_code: String!
-  address_1: String!
-  address_2: String
-  city: String!
-  state: String!
-  postal_code: String!
-  country: String!
-  unlimited_tier: Boolean!
-  credits: Int!
-  users: [User!]!
-  customers: [Customer!]
-  invoices: [Invoice!]
-}
+  type Company {
+    _id: ID!
+    name: String!
+    email: String!
+    phoneNumber: String!
+    address1: String!
+    address2: String
+    zipCode: String!
+    city: String!
+    state: String!
+    premium: Boolean!
+    premiumExpiresOn: String
+    users: [User!]!
+    customers: [Customer!]
+    invoices: [Invoice!]
+    items: [Item!]
+  }
 
-type Customer {
-  _id: ID!
-  name: String!
-  email: String!
-  phone_num: String!
-  country_code: String!
-  address_1: String!
-  address_2: String
-  city: String!
-  state: String!
-  postal_code: String!
-  country: String!
-  companies: [Company!]!
-  invoices: [Invoice!]
-}
+  type Customer {
+    _id: ID!
+    name: String!
+    email: String!
+    phoneNumber: String!
+    address1: String!
+    address2: String
+    zipCode: String!
+    city: String!
+    state: String!
+    companies: [Company!]!
+    invoices: [Invoice!]
+  }
 
+  type Invoice {
+    _id: ID!
+    createdBy: String!
+    number: String!
+    description: String
+    terms: String
+    date: String!
+    dueDate: String!
+    company: Company!
+    customer: Customer!
+    items: [Item!]!
+    subtotal: String!
+    discount: String
+    tax: String
+    shipping: String
+    total: String!
+    balance: String!
+    notes: String
+    paid: Boolean
+  }
 
-type Invoice {
-  _id: ID
-  companyID: String
-  companyName: String
-  userID: String
-  userName: String
-  customerID: String
-  invoiceNumber: String
-  invoiceDescription: String
-  selectedDate: String
-  invoiceDueDate: String
-  company: String
-  addressFrom: String
-  cityTo: String
-  stateTo: String
-  zipCodeTo: String
-  addressTo: String
-  emailTo: String
-  subtotal: String
-  discount: String
-  tax: String
-  shipping: String
-  total: String
-  amountPaid: String
-  balanceDue: String
-  notes: String
-  terms: String
-}
+  input InvoiceInput {
+    createdBy: String!
+    number: String!
+    description: String
+    terms: String
+    date: String!
+    dueDate: String!
+    company: CompanyInput!
+    customer: CustomerInput!
+    items: [ItemInput!]!
+    subtotal: String!
+    discount: String
+    tax: String
+    shipping: String
+    total: String!
+    balance: String!
+    notes: String
+  }
 
-input InvoiceInput {
-  companyID: String!
-  companyName: String!
-  userID: String!
-  userName: String!
-  customerID: String!
-  invoiceNumber: String!
-  invoiceDescription: String!
-  selectedDate: String!
-  invoiceDueDate: String!
-  company: String!
-  addressFrom: String!
-  cityTo: String
-  stateTo: String
-  zipCodeTo: String!
-  addressTo: String
-  emailTo: String!
-  subtotal: String
-  discount: String
-  tax: String!
-  shipping: String!
-  total: String
-  amountPaid: String
-  balanceDue: String!
-  notes: String
-  terms: String
-}
+  input EditInvoiceInput {
+    amountPaid: String!
+  }
 
+  type Item {
+    _id: ID!
+    name: String!
+    description: String
+    quantity: String!
+    cost: String!
+    amount: String!
+  }
 
+  input ItemInput {
+    _id: ID
+    name: String!
+    description: String
+    quantity: String!
+    cost: String!
+    amount: String!
+  }
 
-input EditInvoiceInput {
-  amountPaid: String!
-}
+  input CompanyInput {
+    _id: ID
+    name: String!
+    email: String!
+    phoneNumber: String!
+    address1: String!
+    address2: String
+    zipCode: String!
+    city: String!
+    state: String!
+  }
 
-type Item {
-  _id: ID!
-  description: String!
-  quantity: String!
-  rate: String!
-  amount: String!
-}
+  input EditCompanyInput {
+    _id: ID
+    name: String
+    email: String
+    phoneNumber: String
+    address1: String
+    address2: String
+    zipCode: String
+    city: String
+    state: String
+  }
 
-input ItemInput {
-  _id: ID!
-  description: String!
-  quantity: String!
-  rate: String!
-  amount: String!
-}
+  input UserInput {
+    email: String!
+    name: String!
+    phoneNumber: String!
+  }
 
-input CompanyInput {
-  name: String!
-  email: String!
-  phone_num: String!
-  country_code: String!
-  address_1: String!
-  address_2: String
-  city: String!
-  state: String!
-  postal_code: String!
-  country: String!
-}
+  input EditUserInput {
+    email: String
+    name: String
+    phoneNumber: String
+  }
 
-input EditCompanyInput {
-  _id: ID
-  name: String
-  email: String
-  phone_num: String
-  country_code: String
-  address_1: String
-  address_2: String
-  city: String
-  state: String
-  postal_code: String
-  country: String
-}
+  input CustomerInput {
+    _id: ID
+    name: String!
+    email: String!
+    phoneNumber: String!
+    address1: String!
+    address2: String
+    zipCode: String!
+    city: String!
+    state: String!
+  }
 
-input UserInput {
-  email: String!
-  name: String!
-  phone_num: String!
+  input EditCustomerInput {
+    name: String
+    email: String
+    phoneNumber: String
+    address1: String
+    address2: String
+    zipCode: String
+    city: String
+    state: String
+  }
 
-}
+  type Country {
+    name: String!
+    iso2: String!
+    dialCode: String!
+    priority: Int!
+    format: String
+  }
 
-input EditUserInput {
-  email: String
-  name: String
-  phone_num: String
-}
+  type RootQuery {
+    users: [User!]!
+    user(userId: ID!): User!
+    companyByAnyField(companyInput: EditCompanyInput): [Company!]
+    companies: [Company!]!
+    company(companyId: ID!): Company!
+    customers: [Customer!]!
+    customer(customerId: ID!): Customer!
+    countries: [Country!]!
+    country(name: String, iso2: String): Country!
+    invoices: [Invoice!]!
+    invoice(invoiceId: ID!): Invoice!
+    items: [Item!]!
+    item(itemId: ID!): Item!
+  }
 
-input CustomerInput {
-  name: String!
-  email: String!
-  phone_num: String!
-  country_code: String!
-  address_1: String!
-  address_2: String
-  city: String!
-  state: String!
-  postal_code: String!
-  country: String!
-}
+  type RootMutation {
+    createUser(userInput: UserInput!): User
+    editUser(userId: ID!, editUserInput: EditUserInput!): User
+    createCompany(companyInput: CompanyInput!): Company
+    editCompany(editCompanyInput: EditCompanyInput!, companyId: ID!): Company
+    createCustomer(customerInput: CustomerInput!): Customer
+    editCustomer(customerId: ID!, editCustomerInput: EditCustomerInput!): Customer
+    addUserToCompany(userId: ID!, companyId: ID!): Company
+    addCustomerToCompany(customerId: ID!, companyId: ID!): Customer
+    createInvoice(invoiceInput: InvoiceInput!): Invoice
+    editInvoice(editInvoiceInput: EditInvoiceInput!, invoiceId: ID!): Invoice
+    buyPlanOrCredits(companyId: ID!, quantity: Int!): Company
+    createItem(itemInput: ItemInput!): Item
+  }
 
-input EditCustomerInput {
-  name: String
-  email: String
-  phone_num: String
-  country_code: String
-  address_1: String
-  address_2: String
-  city: String
-  state: String
-  postal_code: String
-  country: String
-}
-
-type Country {
-  name: String!
-  iso2: String!
-  dialCode: String!
-  priority: Int!
-  format: String
-}
-
-type RootQuery {
-  users: [User!]!
-  user(userID: ID!): User!
-  companyByAnyField(companyInput: EditCompanyInput): [Company!]
-  companies: [Company!]!
-  company(companyID: ID!): Company!
-  customers: [Customer!]!
-  customer(customerID: ID!): Customer!
-  countries: [Country!]!
-  country(name: String, iso2: String): Country!
-  invoices: [Invoice!]!
-  invoice(invoiceID: ID!): Invoice!
-}
-
-type RootMutation {
-  createUser(userInput: UserInput!): User
-  editUser(userID: ID!, editUserInput: EditUserInput!): User
-  createCompany(companyInput: CompanyInput!): Company
-  editCompany(editCompanyInput: EditCompanyInput!, companyID: ID!): Company
-  createCustomer(customerInput: CustomerInput!): Customer
-  editCustomer(customerID: ID!, editCustomerInput: EditCustomerInput!): Customer
-  addUserToCompany(userID: ID!, companyID: ID!): Company
-  addCustomerToCompany(customerID: ID!, companyID: ID!): Customer
-  createInvoice(invoiceInput: InvoiceInput!): Invoice
-  editInvoice(editInvoiceInput: EditInvoiceInput!, invoiceID: ID!): Invoice
-  buyPlanOrCredits(companyID: ID!, quantity: Int!): Company
-}
-
-schema {
-  query: RootQuery
-  mutation: RootMutation
-}
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
 `);
