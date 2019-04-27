d2761f5a905cc9ebe0c053717dbf62b8f470a403
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-04-27 18:11:18 -0400

    added styling

--- client/src/components/Settings/styles.js
+++ client/src/components/Settings/styles.js
@@ -1,19 +1,34 @@
 export default theme => ({
-  container : {
-    margin: `0 auto`
+  settingsContainer : {
+    width: `100%`,
+    display: `flex`,
+    padding: theme.spacing.unit * 2,
+    flexDirection: `column`,
+    justifyContent: `space-around`,
+    alignItems: `center`,
+  },
+
+  cardContainer: {
+    maxWidth: `960px`,
+    width: `100%`,
+    marginBottom: `20px` 
   },
 
   cards : {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
+    textAlign: 'start',
     color: theme.palette.text.secondary,
-    width: '75%',
+    width: '100%',
+    height: `100%`,
+    padding: `20px`,
     borderRadius: 0,
-    boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'
+    boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)',
+    display: `flex`,
+    flexDirection: `column`,
+    alignItems: `center`
   },
 
   companyDropDown : {
-    width: `75%`,
+    width: `50%`,
     marginBottom: `20px`,
     fontSize: `2rem`
   }
