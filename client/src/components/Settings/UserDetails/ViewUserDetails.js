e78a5b33e33803186f9575c1056b18af71349de3
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-04-27 18:08:54 -0400

    made changes to the view

--- client/src/components/Settings/UserDetails/ViewUserDetails.js
+++ client/src/components/Settings/UserDetails/ViewUserDetails.js
@@ -4,25 +4,27 @@ import Edit from '../EditIcon'
 export default function ViewUserDetails(props) {
   const {name, email, phone_num } = props.userState
   return (
-    <div className='user-details'>
+    <div className='cardDetails'>
       <div className='card-header'>
         <h1>User Details</h1>
         <div onClick={props.toggleView} className='edit'>
         <Edit />
+      </div>
+    </div>
+    <div className='infoContainer'>
+        <div className='info'>
+          <h2>Name: </h2>
+          <p>{name}</p>
+        </div>
+        <div className='info'>
+          <h2>Email: </h2>
+          <p>{email}</p>
+        </div>
+        <div className='info'>
+          <h2>Phone Number: </h2>
+          <p>{phone_num}</p>
         </div>
       </div>
-      <h2>Name</h2>
-        <p>
-          {name}
-        </p>
-        <h2>Email</h2>
-        <p>
-          {email}
-        </p>
-        <h2>Phone Number</h2>
-        <p>
-          {phone_num}
-        </p>
     </div>
   )
 }
