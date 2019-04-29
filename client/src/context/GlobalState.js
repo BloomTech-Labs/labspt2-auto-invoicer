03d8982be9c386129c91e840b91a8c4fa920f252
Andres Rivera <andres.rivera.toro@gmail.com>
2019-04-29 00:09:18 -0400

    Add getCompanies to GlobalState

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,10 +1,10 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import axios from 'axios';
 
 import UserContext from './UserContext';
 
-import { userReducer, GET_USER } from './reducers';
-import { userData } from './graphql';
+import { userReducer, GET_USER, GET_COMPANIES } from './reducers';
+import { userData, companyData } from './graphql';
 
 const GlobalState = props => {
   const [userState, dispatch] = useReducer(userReducer, {
@@ -42,8 +42,36 @@ const GlobalState = props => {
     dispatch({ type: GET_USER, user: userDetails.data.data.user });
   };
 
+  const getCompanies = async () => {
+    const companiesQuery = {
+      query: `
+        query {
+          user(userId: "${userState.user._id}") {
+            companies {
+              ${companyData}
+            }
+          }
+        }
+      `
+    };
+    const companies = await axios.post(
+      `${process.env.REACT_APP_BACKEND_URL}/graphql`,
+      companiesQuery
+    );
+    dispatch({
+      type: GET_COMPANIES,
+      companies: companies.data.data.user.companies
+    });
+  };
+
+  useEffect(() => {
+    console.log('[userState in GlobalState]: ', userState);
+  }, [userState]);
+
   return (
-    <UserContext.Provider value={{ user: userState.user, getUser }}>
+    <UserContext.Provider
+      value={{ user: userState.user, getUser, getCompanies }}
+    >
       {props.children}
     </UserContext.Provider>
   );
