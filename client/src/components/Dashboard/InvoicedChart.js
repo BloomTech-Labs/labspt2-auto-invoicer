28ce378ae36b779d99ee32a6bbf3620776b40104
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-05-02 22:15:17 -0500

    refactored CustomizedLabel to functional component

--- client/src/components/Dashboard/InvoicedChart.js
+++ client/src/components/Dashboard/InvoicedChart.js
@@ -11,17 +11,15 @@ import {
   ResponsiveContainer
 } from 'recharts';
 
-class CustomizedLabel extends PureComponent {
-  render() {
-    const { x, y, stroke, value } = this.props;
+const CustomizedLabel = props => {
+  const { x, y, stroke, value } = props;
 
-    return (
-      <text x={x} y={y} dy={-4} fill={stroke} fontSize={14} textAnchor="middle">
-        {value}
-      </text>
-    );
-  }
-}
+  return (
+    <text x={x} y={y} dy={-4} fill={stroke} fontSize={14} textAnchor="middle">
+      {value}
+    </text>
+  );
+};
 
 class CustomizedAxisTick extends PureComponent {
   render() {
