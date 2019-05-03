b91c52fa8df6f393840d3e17e04a99df80a73f2c
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-05-02 22:37:12 -0500

    refactored invoicedChart to use hooks

--- client/src/components/Dashboard/InvoicedChart.js
+++ client/src/components/Dashboard/InvoicedChart.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-multi-comp */
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   LineChart,
   Line,
@@ -41,119 +41,114 @@ const CustomizedAxisTick = props => {
   );
 };
 
-export default class InvoicedChart extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: []
-    };
-  }
-  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/5br7g9d6/';
-  componentDidUpdate(prevProps) {
-    if (
-      JSON.stringify(prevProps.invoices) !== JSON.stringify(this.props.invoices)
-    ) {
-      const mappedInvoices = [];
-      let invoicedJanuary = 0;
-      let invoicedFebruary = 0;
-      let invoicedMarch = 0;
-      let invoicedApril = 0;
-      let invoicedMay = 0;
-      let invoicedJune = 0;
-      let invoicedJuly = 0;
-      let invoicedAugust = 0;
-      let invoicedSeptember = 0;
-      let invoicedOctober = 0;
-      let invoicedNovember = 0;
-      let invoicedDecember = 0;
-      this.props.invoices.map(invoice => {
-        if (invoice.date.includes('Jan')) {
-          invoicedJanuary += parseFloat(invoice.total);
-        }
-        if (invoice.date.includes('Feb')) {
-          invoicedFebruary += parseFloat(invoice.total);
-        }
-        if (invoice.date.includes('Mar')) {
-          invoicedMarch += parseFloat(invoice.total);
-        }
-        if (invoice.date.includes('Apr')) {
-          invoicedApril += parseFloat(invoice.total);
-        }
-        if (invoice.date.includes('May')) {
-          invoicedMay += parseFloat(invoice.total);
-        }
-        if (invoice.date.includes('Jun')) {
-          invoicedJune += parseFloat(invoice.total);
-        }
-        if (invoice.date.includes('Jul')) {
-          invoicedJuly += parseFloat(invoice.total);
-        }
-        if (invoice.date.includes('Aug')) {
-          invoicedAugust += parseFloat(invoice.total);
-        }
-        if (invoice.date.includes('Sep')) {
-          invoicedSeptember += parseFloat(invoice.total);
-        }
-        if (invoice.date.includes('Oct')) {
-          invoicedOctober += parseFloat(invoice.total);
-        }
-        if (invoice.date.includes('Nov')) {
-          invoicedNovember += parseFloat(invoice.total);
-        }
-        if (invoice.date.includes('Dec')) {
-          invoicedDecember += parseFloat(invoice.total);
-        }
-        return invoice;
-      });
+const InvoicedChart = props => {
+  const [data, setData] = useState([]);
 
-      mappedInvoices.push(
-        { name: 'Jan', invoiced: invoicedJanuary },
-        { name: 'Feb', invoiced: invoicedFebruary },
-        { name: 'Mar', invoiced: invoicedMarch },
-        { name: 'Apr', invoiced: invoicedApril },
-        { name: 'May', invoiced: invoicedMay },
-        { name: 'Jun', invoiced: invoicedJune },
-        { name: 'Jul', invoiced: invoicedJuly },
-        { name: 'Aug', invoiced: invoicedAugust },
-        { name: 'Sep', invoiced: invoicedSeptember },
-        { name: 'Oct', invoiced: invoicedOctober },
-        { name: 'Nov', invoiced: invoicedNovember },
-        { name: 'Dec', invoiced: invoicedDecember }
-      );
-      this.setState({
-        data: [...mappedInvoices]
-      });
-    }
-  }
-  render() {
-    return (
-      <div style={{ width: '100%', height: 270 }}>
-        <ResponsiveContainer>
-          <LineChart
-            width={700}
-            height={240}
-            data={this.state.data}
-            margin={{
-              top: 20,
-              right: 30,
-              left: 20,
-              bottom: 10
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" height={60} tick={<CustomizedAxisTick />} />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line
-              type="monotone"
-              dataKey="invoiced"
-              stroke="#8884d8"
-              label={<CustomizedLabel />}
-            />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
+  const { invoices } = props;
+
+  useEffect(() => {
+    const mappedInvoices = [];
+
+    let invoicedJanuary = 0;
+    let invoicedFebruary = 0;
+    let invoicedMarch = 0;
+    let invoicedApril = 0;
+    let invoicedMay = 0;
+    let invoicedJune = 0;
+    let invoicedJuly = 0;
+    let invoicedAugust = 0;
+    let invoicedSeptember = 0;
+    let invoicedOctober = 0;
+    let invoicedNovember = 0;
+    let invoicedDecember = 0;
+
+    invoices.map(invoice => {
+      if (invoice.date.includes('Jan')) {
+        invoicedJanuary += parseFloat(invoice.total);
+      }
+      if (invoice.date.includes('Feb')) {
+        invoicedFebruary += parseFloat(invoice.total);
+      }
+      if (invoice.date.includes('Mar')) {
+        invoicedMarch += parseFloat(invoice.total);
+      }
+      if (invoice.date.includes('Apr')) {
+        invoicedApril += parseFloat(invoice.total);
+      }
+      if (invoice.date.includes('May')) {
+        invoicedMay += parseFloat(invoice.total);
+      }
+      if (invoice.date.includes('Jun')) {
+        invoicedJune += parseFloat(invoice.total);
+      }
+      if (invoice.date.includes('Jul')) {
+        invoicedJuly += parseFloat(invoice.total);
+      }
+      if (invoice.date.includes('Aug')) {
+        invoicedAugust += parseFloat(invoice.total);
+      }
+      if (invoice.date.includes('Sep')) {
+        invoicedSeptember += parseFloat(invoice.total);
+      }
+      if (invoice.date.includes('Oct')) {
+        invoicedOctober += parseFloat(invoice.total);
+      }
+      if (invoice.date.includes('Nov')) {
+        invoicedNovember += parseFloat(invoice.total);
+      }
+      if (invoice.date.includes('Dec')) {
+        invoicedDecember += parseFloat(invoice.total);
+      }
+      return invoice;
+    });
+
+    mappedInvoices.push(
+      { name: 'Jan', invoiced: invoicedJanuary },
+      { name: 'Feb', invoiced: invoicedFebruary },
+      { name: 'Mar', invoiced: invoicedMarch },
+      { name: 'Apr', invoiced: invoicedApril },
+      { name: 'May', invoiced: invoicedMay },
+      { name: 'Jun', invoiced: invoicedJune },
+      { name: 'Jul', invoiced: invoicedJuly },
+      { name: 'Aug', invoiced: invoicedAugust },
+      { name: 'Sep', invoiced: invoicedSeptember },
+      { name: 'Oct', invoiced: invoicedOctober },
+      { name: 'Nov', invoiced: invoicedNovember },
+      { name: 'Dec', invoiced: invoicedDecember }
     );
-  }
-}
+
+    setData([...mappedInvoices]);
+  }, [invoices]);
+
+  return (
+    <div style={{ width: '100%', height: 270 }}>
+      <ResponsiveContainer>
+        <LineChart
+          width={700}
+          height={240}
+          data={data}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 10
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" height={60} tick={<CustomizedAxisTick />} />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="invoiced"
+            stroke="#8884d8"
+            label={<CustomizedLabel />}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default InvoicedChart;
