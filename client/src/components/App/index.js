da6ca87f74351f7f14d256fa70db8e3d9113d648
Dewayne87 <dewaynelindsay87@gmail.com>
2019-05-08 04:21:34 -0700

    fixed changed inovice pdf names

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,24 +1,24 @@
-import React, { useState, useEffect, useContext } from 'react';
-import axios from 'axios';
-import { Route, withRouter } from 'react-router-dom';
-import { saveAs } from 'file-saver';
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { Route, withRouter } from "react-router-dom";
+import { saveAs } from "file-saver";
 
-import LandingPage from '../../views/LandingPage';
-import BillingPage from '../../views/BillingPage';
-import CreateInvoice from '../../views/CreateInvoice';
-import SettingsPage from '../../views/SettingsPage';
-import InvoiceList from '../../views/InvoiceList';
-import InvoiceView from '../../views/InvoiceView';
-import SignInModal from '../SignInModal';
-import EditInvoiceForm from '../EditInvoiceForm';
-import Dashboard from '../Dashboard';
-import Navigation from '../Navigation/Navigation';
-import SignUpStepper from '../SignUpStepper/';
+import LandingPage from "../../views/LandingPage";
+import BillingPage from "../../views/BillingPage";
+import CreateInvoice from "../../views/CreateInvoice";
+import SettingsPage from "../../views/SettingsPage";
+import InvoiceList from "../../views/InvoiceList";
+import InvoiceView from "../../views/InvoiceView";
+import SignInModal from "../SignInModal";
+import EditInvoiceForm from "../EditInvoiceForm";
+import Dashboard from "../Dashboard";
+import Navigation from "../Navigation/Navigation";
+import SignUpStepper from "../SignUpStepper/";
 
-import UserContext from '../../context/UserContext';
-import './App.css';
-import Error404Page from './../Errors/404/404';
-import Error500Page from './../Errors/500/500';
+import UserContext from "../../context/UserContext";
+import "./App.css";
+import Error404Page from "./../Errors/404/404";
+import Error500Page from "./../Errors/500/500";
 
 const App = props => {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -35,7 +35,7 @@ const App = props => {
 
   useEffect(() => {
     getUser();
-    console.log('this is my context', context);
+    console.log("this is my context", context);
   }, [loggedIn]);
 
   const toggleAuthModal = () => {
@@ -53,31 +53,23 @@ const App = props => {
       })
       .then(() => {
         setLoggedIn(!loggedIn);
-        window.location.replace('/');
+        window.location.replace("/");
       })
       .catch(err => console.log(err));
   };
 
   const createPDF = invoice => {
     const file = {
-      addressFrom: invoice.addressFrom,
-      addressTo: invoice.addressTo,
-      amountPaid: invoice.amountPaid,
-      balanceDue: invoice.balanceDue,
-      stateTo: invoice.stateTo,
-      zipCodeTo: invoice.zipCodeTo,
-      cityTo: invoice.cityTo,
-      emailTo: invoice.emailTo,
-      selectedDate: invoice.selectedDate,
+      company: invoice.company,
+      customer: invoice.customer,
+      balance: invoice.balance,
+      date: invoice.date,
       discount: invoice.discount,
-      invoiceDueDate: invoice.invoiceDueDate,
-      invoiceDescription: invoice.invoiceDescription,
-      company: invoice.companyName,
-      // invoiceItems: [
-      //   { amount: "10.00", item: "BELL", quantity: "10", rate: "1.00" }
-      // ],
+      dueDate: invoice.dueDate,
+      description: invoice.description,
+      items: invoice.items,
       notes: invoice.notes,
-      invoiceNumber: invoice.invoiceNumber,
+      number: invoice.number,
       terms: invoice.terms,
       shipping: invoice.shipping,
       subtotal: invoice.subtotal,
@@ -85,19 +77,19 @@ const App = props => {
       total: invoice.total
     };
     axios
-      .post('https://pdf-server-invoice.herokuapp.com/create-pdf', file)
+      .post("https://pdf-server-invoice.herokuapp.com/create-pdf", file)
       .then(() =>
-        axios.get('https://pdf-server-invoice.herokuapp.com/fetch-pdf', {
-          responseType: 'blob'
+        axios.get("https://pdf-server-invoice.herokuapp.com/fetch-pdf", {
+          responseType: "blob"
         })
       )
       .then(res => {
-        const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
-        saveAs(pdfBlob, `${file.invoiceNumber}-invoice.pdf`);
+        const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+        saveAs(pdfBlob, `${file.number}-invoice.pdf`);
       })
       .catch(err => {
         console.log(err);
-        return 'Error';
+        return "Error";
       });
   };
 
