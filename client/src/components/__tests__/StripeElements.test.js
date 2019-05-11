5312d8bda4dc995a5e5b7b784dd911be0258bb4d
paulapivat <paul.apivat@gmail.com>
2019-05-11 13:07:41 +0700

    add basic stests for StripeElements

---
+++ client/src/components/__tests__/StripeElements.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+//import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+
+import StripeElements from "../StripeElements/index";
+import StripeCheckoutForm from "../StripeCheckoutForm/index";
+import Typography from "@material-ui/core/Typography";
+//import StripeProvider from "react-stripe-elements";
+
+// NOTE: test checks the StripeElements contains StripeCheckoutForm & Typography
+// test cannot check StripeProvider - TypeError: expects a string, object or component constructor
+
+it("contains StripeCheckoutForm component", () => {
+  const wrapped = shallow(<StripeElements />);
+  expect(wrapped.find(StripeCheckoutForm).length).toEqual(1);
+});
+
+it("contains Typography from Material-UI", () => {
+  const wrapped = shallow(<StripeElements />);
+  expect(wrapped.find(Typography).length).toEqual(1);
+});
