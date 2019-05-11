34223593d7376b0af79757556735b02a3110c180
paulapivat <paul.apivat@gmail.com>
2019-05-11 11:43:33 +0700

    add beforeEach to destructure each test

--- client/src/components/__tests__/App.test.js
+++ client/src/components/__tests__/App.test.js
@@ -10,38 +10,37 @@ import InvoiceList from "../../views/InvoiceList";
 import InvoiceView from "../../views/InvoiceView";
 import EditInvoiceForm from "../EditInvoiceForm";
 
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
 it("shows a sign-in modal", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(SignInModal).length).toEqual(0);
 });
 
 it("contains a navigation component", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(Navigation).length).toEqual(0);
 });
 
 //makes more sense to do: expect(wrapped.find(LandingPage));
 it("contains LandingPage Views component", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(LandingPage).length).toEqual(0);
 });
 
 it("contains CreateInvoice views component", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CreateInvoice));
 });
 
 it("contains InvoiceList views component", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(InvoiceList));
 });
 
 it("contains InvoiceView views component", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(InvoiceView));
 });
 
 it("contains EditInvoiceForm component", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(EditInvoiceForm));
 });
