21b8b55d41010d004e799a80a39f7a5d97705cbc
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-05-11 22:20:45 -0400

    replaced the nav bar with a more modern look and made the company context

---
+++ client/src/components/Navigation/NavLinksBar.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom'
+
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import ToolBar from '@material-ui/core/Toolbar'
+
+import UserContext from '../../context/UserContext';
+import style from './styles'
+
+const NavNavLinksBar = props => {
+  const context = useContext(UserContext)
+  const { classes } = props
+  return (
+    <ToolBar className={classes.navLinksContainer}>
+      <NavLink className="nav_link" to={`/user/${context.user._id}/dashboard`}>
+        <span>Dashboard</span>
+      </NavLink>
+      <NavLink className="nav_link" to={`/user/${context.user._id}/invoices`}>
+      <span>Invoices</span>
+      </NavLink>
+      <NavLink className="nav_link" to={`/user/${context.user._id}/billing`}>
+      <span>Billing</span>
+      </NavLink>
+    </ToolBar>
+  )
+}
+
+export default withStyles(style)(NavNavLinksBar)
--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,60 +1,31 @@
 import React, {useContext} from 'react';
-import { Link } from 'react-router-dom';
+
+import AppBar from '@material-ui/core/AppBar';
+import { withStyles } from '@material-ui/core/styles'
 
 import UserContext from '../../context/UserContext'
-import MyAutoInvoicerIcon from '../../assets/myai.png';
+import AccountBar from './AccountBar'
+import NavLinksBar from './NavLinksBar';
 
 import './Navigation.css';
+import style from './styles'
 
 
 const Navigation = props => {
   const context = useContext(UserContext)
+  const { classes } = props;
   return (
-    <div className="nav-container">
-      <div className="nav">
-        <div className="nav-left">
-          {props.loggedIn ? (
-            <Link to={`/user/${context.user._id}/dashboard`}>
-              <img alt="" src={MyAutoInvoicerIcon} width="32" height="32" />
-              <span className="company-name">myAutoInvoicer</span>
-            </Link>
-          ) : (
-            <Link to="/">
-              <img alt="" src={MyAutoInvoicerIcon} width="32" height="32" />
-              <span className="company-name">myAutoInvoicer</span>
-            </Link>
-          )}
-        </div>
-        {props.loggedIn ? (
-          <div className="nav-right">
-            <Link to={`/user/${context.user._id}/dashboard`}>
-              <span className="link">Dashboard</span>
-            </Link>
-            <Link to={`/user/${context.user._id}/invoices`}>
-              <span className="link">Invoices</span>
-            </Link>
-            <Link to={`/user/${context.user._id}/billing`}>
-              <span className="link">Billing</span>
-            </Link>
-            <Link to={`/user/${context.user._id}/settings`}>
-              <span className="link">Settings</span>
-            </Link>
-            <span className="link" onClick={props.handleSignOut}>
-              Sign Out
-            </span>
-          </div>
-        ) : (
-          <div className="nav-right">
-            <span className="link">Features</span>
-            <span className="link">Get Started</span>
-            <span className="link" onClick={props.handleSignIn}>
-              Sign In
-            </span>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
+  <AppBar className={classes.mainNavContainer}>
+    <AccountBar 
+      loggedIn={props.loggedIn}
+      handleSignIn={props.handleSignIn}
+      handleSignOut={props.handleSignOut} 
+    />
+    {props.loggedIn ?
+      <NavLinksBar /> :
+    null
+  }
+  </AppBar>
+)};
 
-export default Navigation;
+export default withStyles(style)(Navigation);
