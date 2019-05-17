543b7bb4f7f15638fc3ad458bb161dd94733d327
Andres Rivera <andres.rivera.toro@gmail.com>
2019-05-16 21:08:49 -0400

    Update app to use stepper for creating invoices

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,24 +1,25 @@
-import React, { useState, useEffect, useContext } from "react";
-import axios from "axios";
-import { Route, withRouter } from "react-router-dom";
-import { saveAs } from "file-saver";
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import { Route, withRouter } from 'react-router-dom';
+import { saveAs } from 'file-saver';
 
-import LandingPage from "../../views/LandingPage";
-import BillingPage from "../../views/BillingPage";
-import CreateInvoice from "../../views/CreateInvoice";
-import SettingsPage from "../../views/SettingsPage";
-import InvoiceList from "../../views/InvoiceList";
-import InvoiceView from "../../views/InvoiceView";
-import SignInModal from "../SignInModal";
-import EditInvoiceForm from "../EditInvoiceForm";
-import Dashboard from "../Dashboard";
-import Navigation from "../Navigation/Navigation";
-import SignUpStepper from "../SignUpStepper/";
+import LandingPage from '../../views/LandingPage';
+import BillingPage from '../../views/BillingPage';
+import CreateInvoice from '../../views/CreateInvoice';
+import SettingsPage from '../../views/SettingsPage';
+import InvoiceList from '../../views/InvoiceList';
+import InvoiceView from '../../views/InvoiceView';
+import SignInModal from '../SignInModal';
+import EditInvoiceForm from '../EditInvoiceForm';
+import Dashboard from '../Dashboard';
+import Navigation from '../Navigation/Navigation';
+import SignUpStepper from '../SignUpStepper/';
+import CreateInvoiceStepper from '../CreateInvoiceStepper';
 
-import UserContext from "../../context/UserContext";
-import "./App.css";
-import Error404Page from "./../Errors/404/404";
-import Error500Page from "./../Errors/500/500";
+import UserContext from '../../context/UserContext';
+import './App.css';
+import Error404Page from './../Errors/404/404';
+import Error500Page from './../Errors/500/500';
 
 const App = props => {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -35,7 +36,7 @@ const App = props => {
 
   useEffect(() => {
     getUser();
-    console.log("this is my context", context);
+    console.log('this is my context', context);
   }, [loggedIn]);
 
   const toggleAuthModal = () => {
@@ -53,7 +54,7 @@ const App = props => {
       })
       .then(() => {
         setLoggedIn(!loggedIn);
-        window.location.replace("/");
+        window.location.replace('/');
       })
       .catch(err => console.log(err));
   };
@@ -77,19 +78,19 @@ const App = props => {
       total: invoice.total
     };
     axios
-      .post("https://pdf-server-invoice.herokuapp.com/create-pdf", file)
+      .post('https://pdf-server-invoice.herokuapp.com/create-pdf', file)
       .then(() =>
-        axios.get("https://pdf-server-invoice.herokuapp.com/fetch-pdf", {
-          responseType: "blob"
+        axios.get('https://pdf-server-invoice.herokuapp.com/fetch-pdf', {
+          responseType: 'blob'
         })
       )
       .then(res => {
-        const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+        const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
         saveAs(pdfBlob, `${file.number}-invoice.pdf`);
       })
       .catch(err => {
         console.log(err);
-        return "Error";
+        return 'Error';
       });
   };
 
@@ -117,7 +118,9 @@ const App = props => {
         />
         <Route
           path="/user/:id/invoice/create"
-          render={props => <CreateInvoice {...props} click={createPDF} />}
+          render={props => (
+            <CreateInvoiceStepper {...props} click={createPDF} />
+          )}
         />
         <Route path="/user/:id/settings" component={SettingsPage} />
         <Route exact path="/" render={props => <LandingPage {...props} />} />
@@ -131,11 +134,7 @@ const App = props => {
         />
         <Route
           path="/user/:id/invoice/:invoiceID/edit"
-          render={props => (
-            <EditInvoiceForm
-              {...props}
-            />
-          )}
+          render={props => <EditInvoiceForm {...props} />}
         />
       </section>
     </div>
