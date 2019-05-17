2d789dc020a33c83b47112e8eb6da692b254d1c6
paulapivat <paul.apivat@gmail.com>
2019-05-17 15:55:39 +0700

    edit typography of UserDetails and CompanyDetails of settings

--- client/src/components/Settings/UserDetails/EditUserDetails.js
+++ client/src/components/Settings/UserDetails/EditUserDetails.js
@@ -1,62 +1,69 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from "react";
 
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-import UserContext from '../../../context/UserContext'
-import { withStyles } from '@material-ui/core';
+import UserContext from "../../../context/UserContext";
+import { withStyles } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
-import style from '../styles'
+import style from "../styles";
 
-const EditUserDetails = (props) => {
-  const context = useContext(UserContext)
-  const {name, email, phoneNumber} = context.user
-  const [userData, setUserData] = useState({name, email, phoneNumber})
+const EditUserDetails = props => {
+  const context = useContext(UserContext);
+  const { name, email, phoneNumber } = context.user;
+  const [userData, setUserData] = useState({ name, email, phoneNumber });
 
   const editUser = async editedData => {
-    await context.updateUser(editedData)
-    props.toggleView()
-  }
+    await context.updateUser(editedData);
+    props.toggleView();
+  };
 
   return (
     <div>
-      <form className='user-form'>
+      <form className="user-form">
         <TextField
-          id={'name'}
-          label={'Name'}
+          id={"name"}
+          label={"Name"}
           fullWidth={true}
-          placeholder={'Name'}
-          name={'name'}
-          onChange={e => setUserData({...userData, name: e.target.value})}
-          value={userData.name} />
+          placeholder={"Name"}
+          name={"name"}
+          onChange={e => setUserData({ ...userData, name: e.target.value })}
+          value={userData.name}
+        />
         <TextField
-          id={'email'}
-          label={'Email'}
+          id={"email"}
+          label={"Email"}
           fullWidth={true}
-          placeholder={'Email'}
-          name={'email'}
-          onChange={e => setUserData({...userData, email: e.target.value})}
-          value={userData.email} />
+          placeholder={"Email"}
+          name={"email"}
+          onChange={e => setUserData({ ...userData, email: e.target.value })}
+          value={userData.email}
+        />
         <TextField
-          id={'phone_num'}
-          label={'Phone Number'}
+          id={"phone_num"}
+          label={"Phone Number"}
           fullWidth={true}
-          placeholder={'Phone Number'}
-          name={'phone_num'}
-          onChange={e => setUserData({...userData, phoneNumber: e.target.value})}
-          value={userData.phoneNumber} />
+          placeholder={"Phone Number"}
+          name={"phone_num"}
+          onChange={e =>
+            setUserData({ ...userData, phoneNumber: e.target.value })
+          }
+          value={userData.phoneNumber}
+        />
       </form>
-      <Button
-        onClick={props.toggleView}>
-        cancel
+      <Button onClick={props.toggleView}>
+        <Typography variant="button" style={{ color: "black" }}>
+          cancel
+        </Typography>
       </Button>
-      <Button
-        onClick={() => editUser(userData)}
-        >
-        save
+      <Button onClick={() => editUser(userData)}>
+        <Typography variant="button" style={{ color: "black" }}>
+          save
+        </Typography>
       </Button>
     </div>
-  )
-}
+  );
+};
 
-export default withStyles(style)(EditUserDetails)
+export default withStyles(style)(EditUserDetails);
